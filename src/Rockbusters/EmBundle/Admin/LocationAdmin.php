409a07228c43d3a6283296cae91f830b1e98b22f
Jan Antl <janantl@gmail.com>
2014-08-18 11:32:06 -0700

    Init commit
    Symfony bundles
    Sonata admin bundle
    FOS User bundle
    
    + RockbustersBundle with Location Entity

---
+++ src/Rockbusters/EmBundle/Admin/LocationAdmin.php
@@ -0,0 +1,41 @@
+<?php
+// src/Acme/DemoBundle/Admin/LocationAdmin.php
+
+namespace Rockbusters\EmBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class LocationAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name', 'text', array('label' => 'Post Title'))
+            //->add('author', 'entity', array('class' => 'Acme\DemoBundle\Entity\User'))
+            ->add('description') //if no type is specified, SonataAdminBundle tries to guess it
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+            ->add('description')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+            ->add('slug')
+            //->add('author')
+        ;
+    }
+}
---
+++ src/Rockbusters/EmBundle/RockbustersEmBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Rockbusters\EmBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RockbustersEmBundle extends Bundle
+{
+}
