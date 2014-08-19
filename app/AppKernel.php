dca989434c48696b801124390a612050301d7215
Jan Antl <janantl@gmail.com>
2014-08-19 16:30:58 -0700

    Add slugabble behavior + edit location entity

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -3,10 +3,9 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-class AppKernel extends Kernel
-{
-    public function registerBundles()
-    {
+class AppKernel extends Kernel {
+
+    public function registerBundles() {
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
@@ -16,31 +15,23 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            
             new Rockbusters\EmBundle\RockbustersEmBundle(),
-            
             new FOS\UserBundle\FOSUserBundle(),
+
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\jQueryBundle\SonatajQueryBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
             
-        // Add your dependencies
-        new Sonata\CoreBundle\SonataCoreBundle(),
-        new Sonata\BlockBundle\SonataBlockBundle(),
-        new Sonata\jQueryBundle\SonatajQueryBundle(),
-        new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-        //...
-
-        // If you haven't already, add the storage bundle
-        // This example uses SonataDoctrineORMAdmin but
-        // it works the same with the alternatives
-        new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
-
-        // Then add SonataAdminBundle
-        new Sonata\AdminBundle\SonataAdminBundle(),    
-             new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
             new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
-                   // Application Bundles
-            // ...
+            // Application Bundles
             new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
-    
-            
+            //sluggabble
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
@@ -53,8 +44,8 @@ class AppKernel extends Kernel
         return $bundles;
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    public function registerContainerConfiguration(LoaderInterface $loader) {
+        $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
     }
+
 }
--- src/Rockbusters/EmBundle/Entity/Location.php
+++ src/Rockbusters/EmBundle/Entity/Location.php
@@ -3,6 +3,7 @@
 namespace Rockbusters\EmBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * Location
@@ -24,9 +25,16 @@ class Location
     /**
      * @var string
      *
-     * @ORM\Column(name="name", type="string", length=255)
+     * @ORM\Column(name="name", type="string", length=64)
      */
     private $name;
+    
+    /**
+    * @Gedmo\Slug(fields={"name"}, updatable=false)
+    * @ORM\Column(length=255, unique=true)
+    */
+    
+    private $slug;    
 
     /**
      * @var string
@@ -88,5 +96,28 @@ class Location
     public function getDescription()
     {
         return $this->description;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     * @return Location
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
     }
 }
